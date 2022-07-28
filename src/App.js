42d9777eb060ecbeae49227ae3bfe90965c5a4c2
Xarizzar <xarizzar@gmail.com>
2022-07-28 06:36:04 +0300

    Fixed homepage to display test button and changed css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePageButtons from './components/buttons/homePageButtons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-body">
+        <h2>Welcome to Xar's (work in progress) page!</h2>
+        <div className='buttons-list'>
+          <HomePageButtons />
+        </div>
+      </div>
     </div>
   );
 }
