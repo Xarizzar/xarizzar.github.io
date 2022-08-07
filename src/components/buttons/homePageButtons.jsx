d2384d61d0a56a8cbd54f341ad9c5ee9b5089c7f
Xarizzar <xarizzar@gmail.com>
2022-08-07 06:12:28 +0300

    Added Splatoon Twitter and updated About Me

--- src/components/buttons/homePageButtons.jsx
+++ src/components/buttons/homePageButtons.jsx
@@ -5,6 +5,7 @@ class HomePageButtons extends Component {
     state = { buttons: [
         { id: "about", buttonText: "About Me", hrefUrl: "/about-me", isLinkFlag: true },
         { id: "twitter", buttonText: "Twitter", hrefUrl: "https://twitter.com/xarizzar", isLinkFlag: false },
+        { id: "twitter_spl", buttonText: "Twitter (Spl clips)", hrefUrl: "https://twitter.com/KuroXar", isLinkFlag: false },
         { id: "youtube", buttonText: "YouTube", hrefUrl: "https://www.youtube.com/channel/UC4iP3qGpEFY-YP9ZsL1ofGA", isLinkFlag: false }
     ] } 
     render() { 
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,7 +1,13 @@
 function AboutMe() {
     return (
         <div>
-            WIP
+            <p>Hi! My name is Xarizzar Phantasma! Some people also call me Kuro (AKA 玄), which is my original character's name!
+                I'm aspiring to be a game developer! I'm already writing a small, custom game engine in C++, along with aiming to
+                get better at art and starting music composing! I also like playing JRPGs, maybe some Final Fantasy XIV for MMOs
+                and also some Splatoon. My favorite games of all time include but are not limited to Trials of Mana (2D/SNES/NS),
+                Chrono series, Final Fantasy series (mostly VI, IX, IV), and more. I'm autistic, so I often have trouble understanding
+                other people's feelings and expressing myself, so I apologize in advance for this.
+            </p>
         </div>
     );
 }
