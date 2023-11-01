8791838fae1e793768e3409428ecceb1d055e4b4
Xarizzar <xarizzar@gmail.com>
2023-11-01 18:08:35 +0200

    Added the guide

--- src/pages/LoT2-Temperature-Stratum-Guide.js
+++ src/pages/LoT2-Temperature-Stratum-Guide.js
@@ -1,6 +1,92 @@
 function LoT2TempStratumGuide() {
     return (
-        <p>Test!</p>
+        <p className="no-whitespace">{`Labyrinth of Touhou 2: Temperature Stratum Guide (WIP)
+
+1st Area:
+
+Northwest Switch: Increase Temp by 20
+Northeast Switch: Increase Temp by 10
+Southeast Switch: Increase Temp by 2
+South Switch: Decrease Temp by 4
+
+West Warp: 61.8 to 62.2
+West Most Warp: 40.0 to 45.0
+Northwest Warp: 77.0 to 82.0
+Northeast Warp: 47.0 to 48.8
+East Warp: 61.8 to 62.2
+East Most Warp: 70.0 to 75.0
+
+RESET TEMPERATURE TO 50. Hit the Northwest switch. Go to the eastern most Warp. Hit the switch there. Hit the Northwest switch again. Now hit the Northeastern switch. Go to the East Warp. Hit the switch. Return to Gensokyo. Hit the Northwestern switch. Go to the Northwestern warp. Hit the Northeastern and Northwestern switches. Go to the Northeast Warp.
+
+2nd area (eastern part of F13):
+
+Current temperature: 48.0
+
+Switches:
+    2
+
+1		3
+
+Switch 1: Increase Temp by 10
+Switch 2: Increase Temp by 18
+Switch 3: Decrease Temp by 6
+
+Warp to 14F stairs (Northwest warp): 59.8 to 60.2
+Northwestern most warp to chest: 68.0 to 74.0
+Northern most warp: 73.2 to 78.0
+Warp to the right of the switches: 50.6 to 53.2
+
+Hit switches 1 and 3 to get the chest to the right of the switches. Hit switch 2 to open up the Northwestern most warp. Hit Switch 3 again to open up the Northern most warp. Hit switch 1 and 3 again to unlock the Warp to 14F stairs. Go to F14 and hit the relay point, then return to F13 for the rest of the areas. Hit switch 2 and return to Gensokyo. From F13's relay point (1st area), go to the Western most warp and get the chest.
+
+F14 (Center East):
+
+Go to the northeastern hole and fall for an item on F13. Go to the far east hole for an item on F13. Go to the southern most hole for an item on F13. Beat the boss on F14.
+
+F13:
+
+RESET THE TEMPERATURE. Hit the eastern switch on the 1st area. Go to the 2nd area via the F14 relay point and hit all 3 switches. Now go to the north western warp on the 1st area.
+
+F13 (3rd area):
+
+Current temperature: 82.0
+
+Switches:
+    5
+
+4		6
+
+Switch 4: Increase temperature by 8
+Switch 5: Decrease temperature by 7
+Switch 6: Decrease temperature by 5
+West Switch: Decrease temperature by 10
+
+Northern Warp: Accessible with 82 temperature
+Eastern warp: 66.0 to 68.0
+Northeastern Warp: 64.0 to 65.2
+Northern most warp: 47.0 to 50.8
+Southwestern warp: 57.0 to 59.9
+Western warp: 87.0 to 90.0
+Northwestern warp: 67.2 to 69.8
+
+Hit switch 4, go to the Western warp and hit the switch. Hit Switch 4 and 6. Go to the Eastern Warp for a treasure. Hit switches 5 and 4. Go to the northwestern warp for treasure. Hit switches 4 and 6. Go to the Northeastern warp and go to F14 and hit the relay point.
+
+F14 (Center West):
+
+Switch left of relay point: Increase temperature by 7
+Switch right of relay point: Decrease temperature by 10
+Switch to the west: Increase temperature by 15
+
+South Eastern warp: 53.2 to 58.0
+Southern more warp: 30.0 to 35.0
+South warp: 55.6 to 59.0
+Southwest warp: 45.0 to 48.8
+West warp: 38.0 to 42.0
+
+Fall through the middle hole when you reach the 3 south holes for an item on F13. Hit the switch to the right of the relay point to go to the West Warp. Hit the switch to the left of the relay point to unlock the southwestern warp. Hit the switch to the right of the relay point to go to the southern warp, and go to the hole to the right (Reimu's left). Explore until you hit the F15 relay point. Go back to F14. RESET THE TEMPERATURE. Hit the switch to the left of the relay point and go to the south warp for treasure. RESET THE TEMPERATURE. Hit the switch to the right of the relay point and the one to the west to go to the south eastern warp. Fall in the left hole to go to the switch on F13 (Current temperature 50??). Hit it. Then go back to F13 via a hole to the left of the relay point and hit switches 4 and 5. Go to the northern most warp. RESET THE TEMPERATURE. On F14, hit the switch to the right of the relay point and the one to the west, and fall through a hole from the ones to the west of the relay point. Hit switch 4 and go to the Southwestern warp. On F14, go to the southeastern warp. Fall in the right hole to go to F13.
+
+F15:
+
+Fall through the middle hole. Open up the shortcut, and go to F14. RESET THE TEMPERATURE. Go back to that shortcut you opened, and hit the switch there to decrease the temperature by 8. Go to the Center West relay point of F14, and go get the item in the Southern more warp. Back to F15, on the holes west of the relay point (?), fall in the right hole for an item on F14. Fall in the hole for an item on F13. Return to that last hole and go from there, to Floor 15. From the F15 Center Relay Point, fall in the southwest hole to get an item. Back in F15, fall in the northwest hole and go back to F15 from the stairs. Fall in the nearest hole for an item (COMMENT: THIS MIGHT BE WRONG).`}</p>
     );
 }
 
